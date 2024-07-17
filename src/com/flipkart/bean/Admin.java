29f01eb47e70e89e31a5007b23ed2df228f1af01
Ramya-talatam <rxtalatam@gmail.com>
2024-07-17 12:31:37 +0530

    bean changes 2nd time

--- src/com/flipkart/bean/Admin.java
+++ src/com/flipkart/bean/Admin.java
@@ -6,14 +6,15 @@ package com.flipkart.bean;
 /**
  * 
  */
-public class Admin {
+public class Admin extends User{
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+	public Admin() {
+		super();
 	}
+
 
 }
