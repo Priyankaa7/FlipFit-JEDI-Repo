57f1600c817c5209bb00a86b59993513110ab446
Akanksha Mishra <akankshamishra22122001@gmail.com>
2024-07-17 12:01:03 +0530

    Create GymServiceInterface.java

---
+++ src/com/flipkart/business/GymServiceInterface.java
@@ -0,0 +1,22 @@
+package com.flipkart.business;
+
+import java.util.ArrayList;
+
+import com.flipkart.bean.Gym;
+
+public interface GymServiceInterface {
+
+	/**
+	 * Returns Gym details
+	 * @param gymId
+	 * @return Gym
+	 */
+	public Gym viewGym(int gymId);
+
+	/**
+	 * Returns list of gyms
+	 * @return ArrayList
+	 */
+	public ArrayList<Gym> viewGymList();
+
+}
